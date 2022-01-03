6c19d84d231bfb2ffe88139b8fdb3ffc42ef0b25
arun pandian <toarun25@gmail.com>
2022-01-03 23:12:50 +0530

    add dashboard page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Defaultlayout } from './components/layout/Defaultlayout';
+import { Dashboard } from './page/dashboard/Dashboard.page';
 import { Entry } from './page/entry/Entry.page';
 
 function App() {
   return (
     <div className="App">
      {/* <Entry/> */}
-     <Defaultlayout>one of page content using default layout</Defaultlayout>
+     <Defaultlayout>
+       <Dashboard/>
+     </Defaultlayout>
     </div>
   );
 }
